7e47aa26f8a5e2ea7566981a52349acc1271c4a4
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-07-23 12:10:57 -0700

    iter that returns references to nodes

--- src/second.rs
+++ src/second.rs
@@ -2,6 +2,12 @@ pub struct List<T> {
   head: Link<T>, 
 }
 
+pub struct IntoIter<T>(List<T>);
+
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
 type Link<T> = Option<Box<Node<T>>>;
 
 struct Node<T> {
@@ -41,6 +47,14 @@ impl<T> List<T> {
       &mut node.elem
     })
   }
+
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
+
+  pub fn iter(&self) -> Iter<'_, T> {
+    Iter { next: self.head.as_ref().map::<&Node<T>, _>(|node| &node) }
+  }
 }
 
 impl<T> Drop for List<T> {
@@ -49,6 +63,23 @@ impl<T> Drop for List<T> {
     while let Some(mut boxed_node) = cur_link {
       cur_link = boxed_node.next.take();
     }
+  }
+}
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
+  }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
+      &node.elem
+    })
   }
 }
 
@@ -97,6 +128,34 @@ mod test {
     });
 
     assert_eq!(list.peek(), Some(&42));
+  }
+
+  #[test]
+  fn into_iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
+  }
+
+  #[test]
+  fn iter() {
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
   }
 }
 
